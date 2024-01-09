1f125441769ac65e1b28eae7ef9a44cf415e3e4d
GuillermoMancilla <g.andres.mancilla@gmail.com>
2024-01-09 15:43:13 -0300

    se agrega Hibernateutil

---
+++ src/main/java/com/retailjavafinal/util/HibernateUtil.java
@@ -0,0 +1,47 @@
+package com.retailjavafinal.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Propiedades de conexio de Hibernate
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/proyectofinalretail");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "memo1992");
+
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                //Manejar la construccion de tablas de la base de datos
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+
+                configuration.setProperties(settings);
+                //Asociar las clases
+                //configuration.addAnnotatedClass(Clase.class);// Añade esta línea
+                //Servicio de parametros de conexion
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+                //Crear conexion. Será utilizada por los DAO's
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+}
