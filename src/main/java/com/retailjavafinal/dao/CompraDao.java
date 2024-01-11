daa6d1392876a08102e0eb4a2b4ec1ebadaa188f
malcompozo <malcompozo@gmail.com>
2024-01-11 15:16:32 -0300

    creacion metodos DAO genericos (CRUD + listar + listar por ID)

---
+++ src/main/java/com/retailjavafinal/dao/CompraDao.java
@@ -0,0 +1,71 @@
+package com.retailjavafinal.dao;
+
+import com.retailjavafinal.models.Compra;
+import com.retailjavafinal.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class CompraDao {
+
+    public Compra findById(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Compra.class, id);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public static List<Compra> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Compra").list();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public void insert(Compra compra) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.save(compra);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+
+    public void update(Compra compra) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(compra);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null){
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+
+    public void delete(Compra compra) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(compra);
+            transaction.commit();
+        } catch (Exception ex) {
+            if (transaction != null){
+                transaction.rollback();
+            }
+            ex.printStackTrace();
+        }
+    }
+}
